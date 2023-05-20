e529fd94af205aae9b8b37ffb8e0634462778f29
Sandeep Aggarwal <1342667+sanagrwl@users.noreply.github.com>
2023-05-20 18:39:43 -0500

    Provided ability to list auth methods. Enabled github auth and permission to only update github role

--- main.tf
+++ main.tf
@@ -38,6 +38,15 @@ resource "vault_mount" "psec_kv_engine" {
   }
 }
 
+resource "vault_jwt_auth_backend" "github" {
+    description         = "Demonstration of the Terraform JWT auth backend"
+    path                = "github"
+    oidc_discovery_url  = "https://token.actions.githubusercontent.com"
+    bound_issuer        = "https://token.actions.githubusercontent.com"
+    namespace           = vault_namespace.psec.path_fq
+    type                = "jwt"    
+}
+
 resource "vault_policy" "vault-policy-team-psec" {
   name = "vault-policy-team-psec"
   namespace = vault_namespace.psec.path_fq
@@ -65,8 +74,58 @@ path "auth/token/revoke-self" {
 path "sys/capabilities-self" {
   capabilities = ["update"]
 }
+
+# List auth methods
+path "sys/auth"
+{
+  capabilities = ["read"]
+}
+
 EOT
 }
+
+resource "vault_policy" "github" {
+    name = "github"
+    namespace = vault_namespace.psec.path_fq
+
+    policy = <<EOT
+# permission to read secrets
+path "${vault_mount.psec_kv_engine.path}/*" {
+  capabilities = ["read"]
+}
+
+EOT
+}
+
+resource "vault_policy" "manage-github-role" {
+  name = "vault-policy-team-psec-github-role"
+  namespace = vault_namespace.psec.path_fq
+
+  policy = <<EOT
+path "auth/${vault_jwt_auth_backend.github.path}/role" {
+  capabilities = ["list"]
+}
+
+path "auth/${vault_jwt_auth_backend.github.path}/role/${vault_jwt_auth_backend_role.github.role_name}" {
+  capabilities = ["read", "update"]
+}
+
+EOT
+}
+
+resource "vault_jwt_auth_backend_role" "github" {
+  namespace = vault_namespace.psec.path_fq
+  backend         = vault_jwt_auth_backend.github.path
+  role_name       = "github"
+  token_policies  = [vault_policy.github.name]
+  bound_claims = {
+    repository = "sanagrwl/vault-okta-oidc"
+  }
+  user_claim      = "actor"
+  role_type       = "jwt"
+  token_ttl = "600"
+}
+
 
 resource "vault_identity_group" "psec-team" {
   name     = "psec-team"
@@ -88,38 +147,72 @@ resource "vault_identity_group" "psec-team-ns-group" {
   type = "internal"
   namespace = vault_namespace.psec.path_fq
   member_group_ids = [ vault_identity_group.psec-team.id ]
-  policies = [ vault_policy.vault-policy-team-psec.name ]
+  policies = [ vault_policy.vault-policy-team-psec.name, vault_policy.manage-github-role.name ]
 }
+
+# resource "vault_egp_policy" "valid-github-role" {
+#   namespace = vault_namespace.psec.path_fq
+#   name              = "valid-github-role"
+#   paths             = ["*"]
+#   enforcement_level = "hard-mandatory"
+
+#   policy = <<EOT
+# precond = rule {
+#     request.operation in ["update"] and
+#     strings.has_prefix(request.path, "auth/github/role/github")
+# }
+
+# # Function that validates zip codes
+# validate_github_role = func() {
+
+#   # Print some information about the request
+#   # Note that these messages will only be printed when the policy is violated
+#   print("Namespace path:", namespace.path)
+#   print("Request path:", request.path)
+#   print("Request data:", request.data)
+
+#   return false
+
+# }
+
+# # Main Rule
+# github_role_validated = validate_github_role()
+
+# main = rule when precond {
+#     github_role_validated
+# }
+# EOT
+# }
 
 
 #### End: psec team setup
 
 
-#### Start: admin setup
+# #### Start: admin setup
+# resource "vault_policy" "vault-policy-admin" {
+#   name = "vault-policy-admin"
 
-resource "vault_policy" "vault-policy-admin" {
-  name = "vault-policy-admin"
+#   policy = <<EOT
+# path "*" {
+#     capabilities = ["sudo","read","create","update","delete","list","patch"]
+# }
+# EOT
+# }
 
-  policy = <<EOT
-path "*" {
-    capabilities = ["sudo","read","create","update","delete","list","patch"]
-}
-EOT
-}
+# resource "vault_identity_group" "vault-admin" {
+#   name     = "vault-admin"
+#   type     = "external"
+#   policies = [ vault_policy.vault-policy-admin.name ]
 
-resource "vault_identity_group" "vault-admin" {
-  name     = "vault-admin"
-  type     = "external"
-  policies = [ vault_policy.vault-policy-admin.name ]
+#   metadata = {
+#     responsibility="okta-group-vault-admin"
+#   }
+# }
 
-  metadata = {
-    responsibility="okta-group-vault-admin"
-  }
-}
+# resource "vault_identity_group_alias" "okta-group-vault-admins" {
+#   name           = "okta-group-vault-admins" # this must match okta group
+#   mount_accessor = vault_jwt_auth_backend.okta.accessor
+#   canonical_id   = vault_identity_group.vault-admin.id
+# }
+# #### End: admin setup
 
-resource "vault_identity_group_alias" "okta-group-vault-admins" {
-  name           = "okta-group-vault-admins" # this must match okta group
-  mount_accessor = vault_jwt_auth_backend.okta.accessor
-  canonical_id   = vault_identity_group.vault-admin.id
-}
-#### End: admin setup
